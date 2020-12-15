7dca0793dc83b0f2e70a9b8ba1c604208f30b3b6
avuko <iamavuko@gmail.com>
2020-12-16 00:40:33 +0100

    lots of tweak and playbooks, getting close to full logging

--- ansible/deploy/make.sh
+++ ansible/deploy/make.sh
@@ -9,5 +9,5 @@ pam_randori_la-pam_randori.o
 gcc -shared pam_randori_la-pam_randori.o -lpam_misc -lpam -Wl,\
 -soname -Wl,pam_randori.so -o pam_randori.so
 rm -f pam_randori_la-pam_randori.Tpo pam_randori_la-pam_randori.o
-
-cp pam_randori.so /lib/x86_64-linux-gnu/security/
+chmod ugo-x pam_randori.so
+mv pam_randori.so /lib/x86_64-linux-gnu/security/
